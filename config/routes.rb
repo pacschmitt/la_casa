88e95bc3c7a7f59aaf3d6131e96e1f6d348924ba
Pacome Schmitt <pacschmitt@gmail.com>
2023-09-19 12:44:06 +0200

    rails new with bootstrap & simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+    root to: "pages#home"
+    get "about", to: "pages#about", as: :about
+    get "contact", to: "pages#contact", as: :contact
+  end
 end
